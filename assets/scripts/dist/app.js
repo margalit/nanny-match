d1fc1398c5181dba338bb69fe74a5aa69c82a12b
Ruby Brown <ruby@nannymatch.co>
2018-02-02 11:40:32 +1100

    Updated Apply and 3 other files

--- assets/scripts/dist/app.js
+++ assets/scripts/dist/app.js
@@ -1,1 +1,154 @@
-!function(e){function t(i){if(n[i])return n[i].exports;var a=n[i]={exports:{},id:i,loaded:!1};return e[i].call(a.exports,a,a.exports,t),a.loaded=!0,a.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}([function(e,t,n){"use strict";function i(e){return e&&e.__esModule?e:{default:e}}var a=n(1),o=(i(a),n(2)),l=(i(o),function(){var e=document.querySelector("[data-toggle]"),t=document.querySelector("[data-menu]"),n=function(){t.classList.toggle("sm-hide")};e.addEventListener("click",n)}),s=function(){var e=document.querySelector("[data-nanny-form]");if(e){var t=e.querySelectorAll("[data-field]");e.onsubmit=function(e){e.preventDefault(),c(t)}}},c=function(e){var t=void 0,n=!0;e.forEach(function(e){var i=e.value;switch(""!==e.value&&"Select one"!==e.value||(n=!1),e.dataset.field){case"date-of-birth":t=i;var a=Date.now()-new Date(i).getTime(),o=Math.abs(new Date(a).getUTCFullYear()-1970);o<22&&(n=!1);break;case"visa-status":"permanent"!==i&&(n=!1);break;case"wwcc":"will-not-get-wwcc"===i&&(n=!1);break;case"first-aid":"will-not-get-first-aid"===i&&(n=!1);break;case"commited":"no"===i&&(n=!1)}}),n?window.location.assign("https://airtable.com/shreBpX2w5XcDiImt?prefill_Date%20of%20Birth="+t):window.location.assign("/sorry")};document.addEventListener("DOMContentLoaded",function(){l(),s()})},function(e,t){"use strict";var n="keywcoHiGZUHcYyaO",i=document.getElementById("profiles");i&&fetch("https://api.airtable.com/v0/appJv6x50plgdZjnP/Nannies?api_key="+n+"&maxRecords=3&view=Nanny%20Match%20Availabilities").then(function(e){return e.json()}).then(function(e){var t=e.records.map(function(e){return'\n          <div class="col-4 sm-col-14 sm-mb-4">\n            <div class="overflow-hidden mb-3 sm-flex">\n              <div class="col col-5">\n                <div class="col-12 circle bg-cover bg-center" style="background-image: url('+e.fields["Profile photo"][e.fields["Profile photo"].length-1].thumbnails.large.url+')"></div>\n              </div>\n              <div class="col col-9 f3 sm-f2 sm-my-auto">\n                <div class="bold mt-1">'+e.fields.Name.split(" ")[0]+'</div>\n                <div class="feature">'+e.fields.Location+"</div>\n                <div>Available "+e.fields["Available to start"]+'</div>\n              </div>\n            </div>\n            <div class="f3 sm-f2">'+e.fields.Blurb+"</div>\n          </div>\n        "}).join("");i.innerHTML='\n      <div class="py-5 sm-py-4 mx-auto col-12 lg-col-10 flex sm-block justify-between">\n        '+t+"\n      </div>\n      "})},function(e,t){"use strict";var n=document.getElementById("booking-submit");n&&n.addEventListener("click",function(){gtag_report_conversion(),n.style.display="none"})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _profiles = __webpack_require__(1);
+
+	var _profiles2 = _interopRequireDefault(_profiles);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var setupMenu = function setupMenu() {
+	  var toggle = document.querySelector('[data-toggle]');
+	  var menu = document.querySelector('[data-menu]');
+	  var toggleMenu = function toggleMenu() {
+	    menu.classList.toggle('sm-hide');
+	  };
+	  toggle.addEventListener('click', toggleMenu);
+	};
+
+	var setupForm = function setupForm() {
+	  var form = document.querySelector('[data-nanny-form]');
+	  if (form) {
+	    var fields = form.querySelectorAll('[data-field]');
+	    Array.from(fields).map(function (field) {
+	      field.addEventListener('change', function () {
+	        renderErrorText('');
+	      });
+	    });
+	    form.onsubmit = function (evt) {
+	      evt.preventDefault();
+	      if (fieldsValid(fields)) {
+	        screenResults(fields);
+	      } else {
+	        renderErrorText('Please answer all the questions');
+	      }
+	    };
+	  }
+	};
+
+	var fieldsValid = function fieldsValid(fields) {
+	  return Array.from(fields).reduce(function (valid, field) {
+	    return field.value === 'Select one' ? false : valid;
+	  }, true);
+	};
+
+	var renderErrorText = function renderErrorText(text) {
+	  var errorContainer = document.querySelector('[data-error]');
+	  errorContainer.innerText = text;
+	};
+
+	var screenResults = function screenResults(fields) {
+	  var satisfyCriteria = true;
+	  fields.forEach(function (field) {
+	    var value = field.value;
+
+	    if (field.value === '' || field.value === 'Select one') {
+	      satisfyCriteria = false;
+	    }
+	    switch (field.dataset.field) {
+	      case 'experience':
+	        if (value === 'no-experience') satisfyCriteria = false;
+	        break;
+	      case 'visa-status':
+	        if (value !== 'permanent') satisfyCriteria = false;
+	        break;
+	      case 'wwcc':
+	        if (value === 'will-not-get-wwcc') satisfyCriteria = false;
+	        break;
+	      case 'first-aid':
+	        if (value === 'will-not-get-first-aid') satisfyCriteria = false;
+	        break;
+	      case 'commited':
+	        if (value === 'no') satisfyCriteria = false;
+	        break;
+	    }
+	  });
+	  if (satisfyCriteria) {
+	    window.location.assign('https://airtable.com/shreBpX2w5XcDiImt');
+	  } else {
+	    window.location.assign('/sorry');
+	  }
+	};
+
+	document.addEventListener('DOMContentLoaded', function () {
+	  setupMenu();
+	  setupForm();
+	});
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var AIRTABLE_KEY = 'keywcoHiGZUHcYyaO';
+	var profiles = document.getElementById('profiles');
+
+	if (profiles) {
+	  fetch('https://api.airtable.com/v0/appJv6x50plgdZjnP/Nannies?api_key=' + AIRTABLE_KEY + '&maxRecords=3&view=Nanny%20Match%20Availabilities').then(function (resp) {
+	    return resp.json();
+	  }).then(function (response) {
+	    var profilesMarkup = response.records.map(function (nanny) {
+	      return '\n          <div class="col-4 sm-col-14 sm-mb-4">\n            <div class="overflow-hidden mb-3 sm-flex">\n              <div class="col col-5">\n                <div class="col-12 circle bg-cover bg-center" style="background-image: url(' + nanny.fields['Profile photo'][nanny.fields['Profile photo'].length - 1].thumbnails.large.url + ')"></div>\n              </div>\n              <div class="col col-9 f3 sm-f2 sm-my-auto">\n                <div class="bold mt-1">' + nanny.fields['Name'].split(' ')[0] + '</div>\n                <div class="feature">' + nanny.fields['Location'] + '</div>\n                <div>Available ' + nanny.fields['Available to start'] + '</div>\n              </div>\n            </div>\n            <div class="f3 sm-f2">' + nanny.fields['Blurb'] + '</div>\n          </div>\n        ';
+	    }).join('');
+	    profiles.innerHTML = '\n      <div class="py-5 sm-py-4 mx-auto col-12 lg-col-10 flex sm-block justify-between">\n        ' + profilesMarkup + '\n      </div>\n      ';
+	  });
+	}
+
+/***/ }
+/******/ ]);
